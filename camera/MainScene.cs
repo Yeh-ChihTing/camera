1666cd42ac1f3fc9fcc9ebfa74deaa879d99cb80
yeh chih ting <tony199401@gmail.com>
2021-08-11 11:24:55 +0900

    test

--- camera/MainScene.cs
+++ camera/MainScene.cs
@@ -1665,7 +1665,6 @@ namespace camera
                                 }
                             }
 
-
                             BoxNum++;
                             MyBox box = new MyBox();
                             CutPic.Controls.Add(box);
@@ -1678,6 +1677,11 @@ namespace camera
                             MyBoxList[boxnum].Width = w;
                             MyBoxList[boxnum].Height = h;
                             boxnum++;
+
+
+
+
+
                         }
                         else if (boxnum < Boxlimit && boxnum != 0)
                         {
